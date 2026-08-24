842656298ee97aa455e32e332d045861ad684641
Prasanna Iyappan <named.as.prasanna@gmail.com>
2026-08-24 16:39:39 +0530

    fix(playback): restore correct YouTubeClient constants

--- app/src/main/kotlin/com/music/snugglemusix/utils/YTPlayerUtils.kt
+++ app/src/main/kotlin/com/music/snugglemusix/utils/YTPlayerUtils.kt
@@ -81,20 +81,19 @@ object YTPlayerUtils {
     private val poTokenGenerator = PoTokenGenerator()
 
     
-    private val MAIN_CLIENT: YouTubeClient = com.music.innertube.models.YouTubeClient.ANDROID_TESTSUITE
+    private val MAIN_CLIENT: YouTubeClient = com.music.innertube.models.YouTubeClient.ANDROID_VR_1_43_32
 
     private val METADATA_CLIENT: YouTubeClient = com.music.innertube.models.YouTubeClient.WEB_REMIX
 
     private val STREAM_FALLBACK_CLIENTS: Array<YouTubeClient> = arrayOf(
-        com.music.innertube.models.YouTubeClient.ANDROID_MUSIC,
-        com.music.innertube.models.YouTubeClient.WEB_REMIX,
-        com.music.innertube.models.YouTubeClient.ANDROID_VR_1_43_32,
         com.music.innertube.models.YouTubeClient.ANDROID_VR_1_61_48,
+        com.music.innertube.models.YouTubeClient.ANDROID_VR_NO_AUTH,
+        com.music.innertube.models.YouTubeClient.ANDROID_NO_SDK,
+        com.music.innertube.models.YouTubeClient.WEB_REMIX,
         com.music.innertube.models.YouTubeClient.TVHTML5_SIMPLY_EMBEDDED_PLAYER,
         com.music.innertube.models.YouTubeClient.TVHTML5,
         com.music.innertube.models.YouTubeClient.ANDROID_CREATOR,
         com.music.innertube.models.YouTubeClient.IPADOS,
-        com.music.innertube.models.YouTubeClient.ANDROID_VR_NO_AUTH,
         com.music.innertube.models.YouTubeClient.MOBILE,
         com.music.innertube.models.YouTubeClient.IOS,
         com.music.innertube.models.YouTubeClient.WEB,
