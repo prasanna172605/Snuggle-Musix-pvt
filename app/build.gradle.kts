ddde261e2369ad2d23dd0d36886c4f3e64bf99de
Prasanna Iyappan <named.as.prasanna@gmail.com>
2026-08-24 23:36:07 +0530

    chore(release): bump version to v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.snuggle.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1005
-        versionName = "1.0.3"
+        versionCode = 1006
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
