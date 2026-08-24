bb8bcf2f31b99fdbf11044b08c9bf6bf7a09863d
Prasanna Iyappan <named.as.prasanna@gmail.com>
2026-08-24 16:24:08 +0530

    bump: version 1.0.2 (code 1004)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.snuggle.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1003
-        versionName = "1.0.0"
+        versionCode = 1004
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
